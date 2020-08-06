e358378afa9b5ea471f0fc93a484ec4fd4ebf59f
Dung <trungdungr9000@gmail.com>
2020-08-06 11:28:25 +0700

    For deploy on heroku

--- app.js
+++ app.js
@@ -1,61 +1,64 @@
-const express = require('express')
-const request = require('request')
-const bodyParser = require('body-parser')
-var cors = require('cors')
-var app = express()
-var url = require('url');
-const { json } = require('body-parser');
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+var cors = require("cors");
+var app = express();
+var url = require("url");
+const { json } = require("body-parser");
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
-app.use(cors())
-function fullUrl(req) {
-  return url.format({
-    protocol: req.protocol,
-    host: req.get('host'),
-    pathname: req.originalUrl
-  });
-}
-var server = require("http").createServer(app)
-username = "buitrungdung",
-  password = "Rampage_972",
-  server.listen(8080, (err) => {
-    if (err) console.log("Err: " + err)
-    console.log('Server is running')
-  })
+app.use(cors());
+
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + "/public"));
+
+// set the home page route
+app.get("/", function (req, res) {
+  // ejs render automatically looks in the views folder
+  res.render("index");
+});
+
+app.listen(port, function () {
+  console.log("Our app is running on port" + port);
+})
 var options = {
   method: "POST",
-  url: 'https://jirasoft.vnptmedia.vn/rest/api/2/issue/GPPMTRAIN-28/transitions?expand=transitions.fields',
+  url:
+    "https://jirasoft.vnptmedia.vn/rest/api/2/issue/GPPMTRAIN-28/transitions?expand=transitions.fields",
   headers: {
-    'User-Agent': 'RG',
-    'Content-Type': 'application/json'
+    "User-Agent": "RG",
+    "Content-Type": "application/json",
   },
-  auth: {
-    user: username,
-    password: password
-  },
-}
+};
 
 requestToJira = (options) => {
   return new Promise(function (resolve, reject) {
     request(options, function (error, res, body) {
       if (!error) {
-        console.log("DONE JIRA")
-        resolve(body)
-
+        resolve(body);
       } else {
         reject(error);
       }
     });
   });
-}
+};
 
-app.post('/*', async (res, req) => {
-  let url = res.url.slice(1, res.url.length)
-  options.url = url
-  options.body = JSON.stringify(res.body)
-  await requestToJira(options)
-  console.log("done node")
-  req.send(url)
-})
+app.post("/*", async (res, req) => {
+  let url = res.url.slice(1, res.url.length);
+  options.url = url;
+  options.body = JSON.stringify(res.body);
+  options.headers = {
+    "User-Agent": "RG",
+    "Content-Type": "application/json",
+    authorization: res.headers.authorization,
+  };
+  await requestToJira(options);
+  req.send(url);
+});
